ecb39ef953a7127a1c19d2941ba1fadbf4e2471a
MatiasFHC <u202213515@upc.edu.pe>
2024-09-20 23:28:06 -0500

    Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '580px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
+  },
+  plugins: [],
+}
